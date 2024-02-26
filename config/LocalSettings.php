4a40638d9c2e817e6a4f1ddff5ac136dd5ed9dac
lis <gyaru@users.noreply.github.com>
2024-02-26 21:02:10 +0100

    add cf IPs, add svg support, delete miser mode

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -108,9 +108,6 @@ $wgJobQueueAggregator = [
 	'redisServer' => getenv('REDIS_SERVER'),
 ];
 
-# disable unnecessary db hits
-$wgMiserMode = true;
-
 # add canonical meta tag on every page
 $wgEnableCanonicalServerLink = true;
 
@@ -124,7 +121,9 @@ $wgImageMagickConvertCommand = "/usr/bin/convert";
 $wgNativeImageLazyLoading = true;
 $wgMaxImageArea = 6.4e7; # fix big boi images
 $wgUseInstantCommons = false;
-$wgFileExtensions = [ 'png', 'gif', 'jpg', 'jpeg', 'webp', 'ico', ];
+$wgFileExtensions = [ 'png', 'gif', 'jpg', 'jpeg', 'webp', 'ico', 'svg'];
+$wgAllowTitlesInSVG = true;
+$wgSVGConverter = 'ImageMagick';
 $wgApiFrameOptions = 'SAMEORIGIN';
 
 # disable pingback
@@ -171,7 +170,32 @@ $wgJobRunRate = 0;
 
 # use X-Forward-For
 $wgUsePrivateIPs = true;
-$wgCdnServersNoPurge[] = '10.0.0.0/8';
+$wgCdnServersNoPurge = [
+	'10.0.0.0/8',
+	'173.245.48.0/20',
+	'103.21.244.0/22',
+	'103.22.200.0/22',
+	'103.31.4.0/22',
+	'141.101.64.0/18',
+	'108.162.192.0/18',
+	'190.93.240.0/20',
+	'188.114.96.0/20',
+	'197.234.240.0/22',
+	'198.41.128.0/17',
+	'162.158.0.0/15',
+	'104.16.0.0/13',
+	'104.24.0.0/14',
+	'172.64.0.0/13',
+	'131.0.72.0/22',
+	'2400:cb00::/32',
+	'2606:4700::/32',
+	'2803:f800::/32',
+	'2405:b500::/32',
+	'2405:8100::/32',
+	'2a06:98c0::/29',
+	'2c0f:f248::/32',
+	'2405:b500::/32'
+];
 
 # allow user css/js
 $wgAllowUserCss = true;
