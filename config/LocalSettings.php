6354e24edffd994e92644bcb37211f3113e022a8
lis <gyaru@users.noreply.github.com>
2024-02-27 11:02:47 +0100

    let sysop add interwiki prefixes

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -233,7 +233,8 @@ $wgGroupPermissions['editors']['editprotected'] = true;
 $wgGroupPermissions['editors']['editinterface'] = true;
 # let sysop delete deletelogentry and deleterevision
 $wgGroupPermissions['sysop']['deletelogentry'] = true;
-$wgGroupPermissions['sysop']['deleterevision'] = true;
+# let sysop add interwiki prefixes
+$wgGroupPermissions['sysop']['interwiki'] = true;
 
 # footer
 $wgFooterIcons = [
